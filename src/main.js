a5cb3d7e12509793e90a79c6db5a0e758d9dbd5f
Dima_V <astrallux777@ukr.net>
2025-06-21 20:23:53 +0300

    task-2

--- src/main.js
+++ src/main.js
@@ -1,16 +1,44 @@
-import { getAllProducts } from './requests/products';
+import { getAllProducts, getSingleProduct } from './requests/products';
 
-import { markupAllProducts } from './services/markupService';
+import { markupAllProducts, markupSingleProduct } from './services/markupService';
 
 
 const allProducts = document.querySelector("#allProducts");
 // getAllProducts().then(console.log)
 
+const singleProductForm = document.querySelector("#singleProductForm");
+const singleProduct = document.querySelector("#singleProduct");
+
+
+
+
+
+
 getAllProducts().then(({ data: { products } }) => {
   console.log(products);
-  const markupCards = markupAllProducts(products);
-  allProducts.insertAdjacentHTML('beforeend', markupCards);
+  // const markupCards = markupAllProducts(products);
+  // allProducts.insertAdjacentHTML('beforeend', markupCards);
 });
 
 
+singleProductForm.addEventListener("submit", findProductById)
 
+function findProductById(event) {
+
+event.preventDefault();
+
+const id = event.target.elements.id.value
+// getSingleProduct(id)
+// .then(console.log)
+getSingleProduct(id)
+.then(({data}) => {
+console.log(data);
+
+const markupCard = markupSingleProduct(data)
+
+singleProduct.innerHTML = markupCard;
+
+singleProductForm.reset();
+
+})
+}
--- src/requests/products.js
+++ src/requests/products.js
@@ -3,4 +3,9 @@ import { dummiapy } from "../services/api";
 // запит на всі продукти
 export async function getAllProducts() {
     return await dummiapy.get('/products')
+};
+
+export async function getSingleProduct(id) {
+    return await dummiapy.get(`/products/${id}`)
+    
 }
